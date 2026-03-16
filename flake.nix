9409e703087423d25fa7f075b40c3d4e1c11d1f7
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:27 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -1,31 +1,20 @@
 {
   description = "Akeyless CloudId Provider for authenticating via cloud authorization providers (AWS, Azure, GCP)";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = { url = "github:pleme-io/substrate"; inputs.nixpkgs.follows = "nixpkgs"; };
     flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      packages.default = pkgs.buildNpmPackage {
-        pname = "akeyless-cloud-id";
-        version = "0.0.0-dev";
-        src = self;
-        npmFlags = [ "--legacy-peer-deps" ];
-        npmDepsHash = "sha256-F+ga7VMfOFRrb8zv2CaLqea4NNt0TGTbbolYLbEscXw=";
-        dontNpmBuild = true;
-        meta = {
-          description = "Akeyless CloudId Provider for authenticating via cloud authorization providers (AWS, Azure, GCP)";
-          homepage = "https://github.com/pleme-io/akeyless-js-cloud-id";
-          license = pkgs.lib.licenses.isc;
-        };
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ nodejs_22 ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "npm";
+    builder = "package";
+    pname = "akeyless-cloud-id";
+    npmDepsHash = "sha256-F+ga7VMfOFRrb8zv2CaLqea4NNt0TGTbbolYLbEscXw=";
+    dontNpmBuild = true;
+    npmFlags = [ "--legacy-peer-deps" ];
+    description = "Akeyless CloudId Provider for authenticating via cloud authorization providers (AWS, Azure, GCP)";
+  };
 }
